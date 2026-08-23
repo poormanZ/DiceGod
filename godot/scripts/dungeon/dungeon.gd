2bd5f11bed4db915ab324dc493429bb236403f45
poormanZ <134785517+poormanZ@users.noreply.github.com>
2026-08-23 20:40:24 +0900

    Show persistent run status in dungeon

--- godot/scripts/dungeon/dungeon.gd
+++ godot/scripts/dungeon/dungeon.gd
@@ -16,6 +16,7 @@ extends Control
 @onready var boss_button: Button = $MarginContainer/Content/Map/Row2/BossButton
 @onready var new_run_button: Button = $MarginContainer/Content/NewRunButton
 @onready var status_label: Label = $MarginContainer/Content/StatusLabel
+@onready var run_status_label: Label = $MarginContainer/Content/RunStatusLabel
 
 func _ready() -> void:
 	if not RunState.active_run:
@@ -33,34 +34,35 @@ func _setup_node_buttons() -> void:
 	new_run_button.hide()
 
 func _update_progress() -> void:
+	run_status_label.text = RunState.get_run_summary()
 	if RunState.boss_cleared:
 		_disable_all_nodes()
 		new_run_button.show()
-		status_label.text = "보스를 쓰러뜨렸습니다! 런 클리어!\n%s" % RunState.get_run_summary()
+		status_label.text = "보스를 쓰러뜨렸습니다! 런 클리어!"
 	elif RunState.elite_cleared:
 		_disable_all_nodes()
 		boss_button.disabled = false
-		status_label.text = "엘리트를 돌파했습니다. 마지막 보스에 도전하세요.\n%s" % RunState.get_run_summary()
+		status_label.text = "엘리트를 돌파했습니다. 마지막 보스에 도전하세요."
 	elif RunState.shop_resolved:
 		_disable_all_nodes()
 		elite_button.disabled = false
-		status_label.text = "상점을 통과했습니다. 다음은 엘리트 노드입니다.\n%s" % RunState.get_run_summary()
+		status_label.text = "상점을 통과했습니다. 다음은 엘리트 노드입니다."
 	elif RunState.event_resolved:
 		_disable_all_nodes()
 		shop_button.disabled = false
-		status_label.text = "이벤트를 해결했습니다. 다음은 상점 노드입니다.\n%s" % RunState.get_run_summary()
+		status_label.text = "이벤트를 해결했습니다. 다음은 상점 노드입니다."
 	elif RunState.reward_claimed:
 		_disable_all_nodes()
 		event_button.disabled = false
-		status_label.text = "보상을 획득했습니다. 다음은 이벤트 노드입니다.\n%s" % RunState.get_run_summary()
+		status_label.text = "보상을 획득했습니다. 다음은 이벤트 노드입니다."
 	elif RunState.battle_cleared:
 		_disable_all_nodes()
 		reward_button.disabled = false
-		status_label.text = "전투를 완료했습니다. 보상 노드를 선택하세요.\n%s" % RunState.get_run_summary()
+		status_label.text = "전투를 완료했습니다. 보상 노드를 선택하세요."
 	else:
 		_disable_all_nodes()
 		start_button.disabled = false
-		status_label.text = "일반 전투부터 시작하세요.\n%s" % RunState.get_run_summary()
+		status_label.text = "일반 전투부터 시작하세요."
 
 func _disable_all_nodes() -> void:
 	start_button.disabled = true
