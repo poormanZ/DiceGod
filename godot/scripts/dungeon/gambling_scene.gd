648ca9c5e4a275cba27a99df13a10df1fc04ce6d
poormanZ <134785517+poormanZ@users.noreply.github.com>
2026-08-24 23:13:17 +0900

    feat: return from gambling to dungeon

--- godot/scripts/dungeon/gambling_scene.gd
+++ godot/scripts/dungeon/gambling_scene.gd
@@ -3,3 +3,8 @@ extends GamblingUI
 
 func _ready() -> void:
 	setup(RunState)
+	completed.connect(_return_to_dungeon)
+
+func _return_to_dungeon() -> void:
+	RunState.resolve_event("gamble_done")
+	get_tree().change_scene_to_file("res://scenes/dungeon/dungeon.tscn")
