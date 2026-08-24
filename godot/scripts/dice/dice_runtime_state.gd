2ead5554f8ca8041058fa43a9793e62b025f2a52
poormanZ <134785517+poormanZ@users.noreply.github.com>
2026-08-24 15:25:39 +0900

    주사위 런타임 상태를 심볼 기반으로 변경

--- godot/scripts/dice/dice_runtime_state.gd
+++ godot/scripts/dice/dice_runtime_state.gd
@@ -1,19 +1,22 @@
 class_name DiceRuntimeState
 extends RefCounted
 
-## 전투 중 변하는 개별 주사위의 상태입니다.
+## 전투 중 변하는 개별 심볼 주사위의 상태입니다.
 var dice_data: DiceData
 var result: int = 0
 var is_locked: bool = false
 
-
 func _init(initial_dice_data: DiceData) -> void:
 	dice_data = initial_dice_data
-
 
 func has_result() -> bool:
 	return result != 0
 
+func get_symbol() -> String:
+	return DiceData.symbol_for(result)
+
+func get_name() -> String:
+	return DiceData.name_for(result)
 
 func toggle_lock() -> void:
 	if has_result():
