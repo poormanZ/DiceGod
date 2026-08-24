64493f2f4b73ea013d96e3dd00ea9bc3cd48eb82
poormanZ <134785517+poormanZ@users.noreply.github.com>
2026-08-25 01:39:58 +0900

    cleanup: remove legacy equipment data

--- godot/scripts/character/equipment_data.gd
+++
@@ -1,8 +0,0 @@
-class_name EquipmentData
-extends Resource
-
-## 장비의 변하지 않는 표시 정보입니다.
-@export var display_name: String = ""
-@export_multiline var description: String = ""
-@export var evades_straight_attacks: bool = false
-@export var attack_bonus: int = 0
