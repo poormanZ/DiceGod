452af7a3060aabfd0c580639f747b36094fbabb7
poormanZ <134785517+poormanZ@users.noreply.github.com>
2026-08-24 23:15:27 +0900

    feat: connect elite battle to run flow

---
+++ godot/scripts/battle/elite_run_battle.gd
@@ -0,0 +1,19 @@
+class_name EliteRunBattle
+extends Battle
+
+func _handle_victory() -> void:
+	is_battle_over = true
+	RunState.current_hp = player.current_hp
+	RunState.elite_cleared = true
+	RunState.reward_claimed = false
+	_show_combat_feedback("♛ 엘리트 처치! 골드 보상을 선택하세요.")
+	await get_tree().create_timer(0.6).timeout
+	get_tree().change_scene_to_file("res://scenes/dungeon/reward.tscn")
+
+func _handle_defeat() -> void:
+	is_battle_over = true
+	RunState.current_hp = 0
+	RunState.die()
+	_show_combat_feedback("💀 엘리트에게 패배했습니다.")
+	await get_tree().create_timer(0.7).timeout
+	get_tree().change_scene_to_file("res://scenes/dungeon/reincarnation.tscn")
