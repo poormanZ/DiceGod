351872700d1d60bffd12addc3be6a701ee0d634f
poormanZ <134785517+poormanZ@users.noreply.github.com>
2026-08-24 23:13:07 +0900

    fix: allow multiple forge actions before leaving

--- godot/scripts/dungeon/forge_scene.gd
+++ godot/scripts/dungeon/forge_scene.gd
@@ -4,11 +4,6 @@ extends ForgeUI
 func _ready() -> void:
 	setup(RunState)
 	closed.connect(_return_to_dungeon)
-	forge_completed.connect(_on_forge_completed)
-
-func _on_forge_completed(_die_index: int, _face_index: int, _symbol_id: int) -> void:
-	await get_tree().create_timer(0.5).timeout
-	_return_to_dungeon()
 
 func _return_to_dungeon() -> void:
 	RunState.resolve_event("forge_done")
